d599d4067da3e71e4a72d979262ddf5288ee5e0f
dominicpam89 <dominicpam89@gmail.com>
2024-10-04 21:07:30 +0700

    establish metadata icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,11 @@ export const metadata: Metadata = {
 	title: "Teach and Tackle",
 	description:
 		"Welcome to TeaTac (Teach and Tackle), where students ask the real questions and teachers drop the knowledge bombs—sometimes with a side of sarcasm! Built with Next.js App Router, this is your go-to place for solving academic mysteries, sharing insights, and maybe cracking a joke or two along the way. Whether you're stumped or ready to stump your teacher, TeaTac (Teach and Tackle) is the place to get answers with a smile.",
+	icons: {
+		icon: "/assets/logo-md.png",
+		apple: "/assets/logo-md.png",
+		shortcut: "/assets/logo-md.png",
+	},
 };
 
 export default function RootLayout({
