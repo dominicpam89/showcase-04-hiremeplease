eb74267dad4b37900d4b3696bc141a94aa23e75d
dominicpam89 <dominicpam89@gmail.com>
2024-10-04 15:13:44 +0700

    minor update

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,6 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
 	return (
 		<>
-			<h1 className="heading-1">Sign In Page</h1>
 			<SignIn />
 		</>
 	);
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,6 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
 	return (
 		<>
-			<h1 className="heading-1">Sign Up Page</h1>
 			<SignUp />
 		</>
 	);
