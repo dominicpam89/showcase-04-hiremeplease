ed21fcde3bf571281bd11807616bb6d3e25d3a42
dominicpam89 <dominicpam89@gmail.com>
2024-10-15 18:46:39 +0700

    feat(auth): fix route.ts file that mislocated in other folder

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -1,57 +1,21 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
-// Define the POST handler
 export async function POST(request: NextRequest) {
-	try {
-		// Parse the incoming request body
-		const body = await request.json();
-		const { loggedIn, userId, email } = body;
-
-		// Validate the request data
-		if (typeof loggedIn !== "boolean") {
-			return NextResponse.json(
-				{ message: "Invalid data: 'loggedIn' must be a boolean" },
-				{ status: 400 }
-			);
-		}
-
-		// If the user is logged in, set a session cookie
-		if (loggedIn && userId && email) {
-			// Create a new response with a success message
-			const response = NextResponse.json({
-				message: "Session updated",
-				userId,
-				email,
-			});
-
-			// Set the session cookie with the user ID (HTTP-only)
-			response.cookies.set({
-				name: "auth",
-				value: JSON.stringify({ userId, email }),
+	const { logged, limitedUserInfo } = await request.json();
+	if (logged) {
+		try {
+			const sessionData = JSON.stringify(limitedUserInfo);
+			cookies().set("session-token", sessionData, {
 				httpOnly: true,
+				sameSite: "strict",
 				path: "/",
-				secure: true,
-				sameSite: "lax",
+				maxAge: 3 * 60, // Cookie expiration is set to match the token expiration
 			});
-
-			return response;
-		} else if (!loggedIn) {
-			// If logged out, clear the auth cookie
-			const response = NextResponse.json({ message: "Logged out" });
-			response.cookies.delete("auth");
-			return response;
-		} else {
-			// Handle cases where required fields are missing
-			return NextResponse.json(
-				{ message: "Missing userId or email for logged-in session" },
-				{ status: 400 }
-			);
+		} catch (error) {
+			cookies().delete("session-token");
 		}
-	} catch (error) {
-		console.error("Error handling session:", error);
-		return NextResponse.json(
-			{ message: "Internal Server Error" },
-			{ status: 500 }
-		);
+	} else {
+		cookies().delete("session-token");
 	}
 }
