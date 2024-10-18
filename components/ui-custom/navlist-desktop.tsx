ebf0a4e854df4ca2f1ef239f5879cfd2c1cadd13
dominicpam89 <dominicpam89@gmail.com>
2024-10-18 17:00:20 +0700

    update style all

--- components/ui-custom/navlist-desktop.tsx
+++ components/ui-custom/navlist-desktop.tsx
@@ -1,6 +1,7 @@
 import { getNavList } from "@/constant/nav.constant";
 import { cn } from "@/lib/utils";
 import NavlistItemUI from "./navlist-item";
+import { LogoUIWithLink } from "./logo";
 
 export default function NavlistDesktop() {
 	const navlist = getNavList("md");
@@ -9,11 +10,29 @@ export default function NavlistDesktop() {
 			aria-label="navlist-desktop"
 			className={cn(
 				"max-md:hidden",
-				"w-3/12 p-8",
+				"w-3/12 p-6 flex flex-col gap-8",
 				"shadow-md shadow-primary/10 dark:shadow-primary-foreground/10"
 			)}
 		>
-			<ul aria-label="list" className="flex flex-col gap-5">
+			<div
+				aria-label="navlist-desktop-header"
+				className="flex flex-col gap-3"
+			>
+				<div aria-label="header-title" className="flex items-center gap-3">
+					<LogoUIWithLink size="lg" />
+					<h2 className="font-black text-primary text-xl">Teatac</h2>
+				</div>
+				<div aria-label="header-content">
+					<p className="text-xs text-primary/50 text-justify text-pretty break-all">
+						Welcome to TeaTac (Teach and Tackle), ask the real questions
+						to teachers for knowledge bombs
+					</p>
+				</div>
+			</div>
+			<ul
+				aria-label="navlist-desktop-content"
+				className="flex flex-col gap-5"
+			>
 				{navlist.map((item) => (
 					<NavlistItemUI key={item.link} item={item} />
 				))}
