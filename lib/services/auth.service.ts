07df61d9a102b2d218ef862fb26632384a91a29c
dominicpam89 <dominicpam89@gmail.com>
2024-10-22 10:24:42 +0700

    store user after successfull registration

--- lib/services/auth.service.ts
+++ lib/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { auth } from "@/firebase.config";
+import { auth, db } from "@/firebase.config";
 import {
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
@@ -6,8 +6,30 @@ import {
 	updateProfile,
 	signOut,
 } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 const DOMAIN_URL = process.env.NEXT_PUBLIC_DOMAIN_DEV;
+
+async function storeUserIntoDb(user: {
+	uid: string;
+	email: string;
+	password: string;
+	firstName?: string;
+	lastName?: string;
+}) {
+	try {
+		await setDoc(doc(db, "users", user.uid), {
+			name: {
+				first: user.firstName || "",
+				last: user.lastName || "",
+			},
+			email: user.email,
+		});
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
 
 export async function sessionUpdate(user: User | null) {
 	if (user) {
@@ -65,6 +87,13 @@ export async function registerWithPassword({
 		);
 		await updateProfile(user, {
 			displayName: firstName + " " + lastName,
+		});
+		await storeUserIntoDb({
+			email,
+			password,
+			firstName,
+			lastName,
+			uid: user.uid,
 		});
 		return await sessionUpdate(user);
 	} catch (error) {
